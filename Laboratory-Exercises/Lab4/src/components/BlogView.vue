ad131a89211e05a1f5d268bfa6db67df652ba66e
Daniel Ilievski <dilievski54@gmail.com>
2025-01-26 16:09:16 +0100

    Lab4

---
+++ Laboratory-Exercises/Lab4/src/components/BlogView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="showEditor = !showEditor" v-if="!showEditor">Add new blog</button>
+    <BlogEditor v-else @addNewItem="addItem"/>
+    <Blogs :error="error" :isLoading="isLoading" :blogs="blogs" @deleteBlog="deleteItem" />
+  </div>
+</template>
+<script setup>
+import BlogEditor from "@/components/BlogEditor.vue";
+import {useBlogs} from "@/composables/useBlogs.js";
+import {ref} from "vue";
+import Blogs from "@/components/Blogs.vue";
+
+const {blogs, isLoading, error, addItem, deleteItem} = useBlogs()
+const showEditor = ref(false);
+
+</script>
